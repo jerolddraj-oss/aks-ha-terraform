45f7b3eeffc6afbfcf04dabd0602113df76ab3e8
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-02 12:01:55 +0530

    Update variables.tf

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -3,3 +3,9 @@ variable "location" {}
 variable "resource_group_name" {}
 variable "subnet_id" {}
 variable "log_analytics_id" {}
+
+# ✅ ADD THIS
+variable "vm_size" {
+  description = "VM size for AKS node pool"
+  type        = string
+}
