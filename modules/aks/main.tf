d2d8f8a875d58f469a142cfe9a55ce6a27382d75
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-02 12:02:46 +0530

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name            = "system"
     node_count      = 1
-    vm_size         = "Standard_B2s_v2"
+    vm_size         = var.vm_size
     vnet_subnet_id  = var.subnet_id
   }
 
