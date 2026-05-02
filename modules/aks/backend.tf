f051090be0f533098851d187e793ee62b042084e
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-02 11:06:14 +0530

    fix: remove backend from module (only root allowed)

--- modules/aks/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"        # <-- your RG where storage exists
-    storage_account_name = "jdterraformstate"  # ✅ your storage account
-    container_name       = "tfstate"           # ✅ your container
-  }
-}
