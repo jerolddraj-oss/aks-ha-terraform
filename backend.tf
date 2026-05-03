4a090cc21ed331af77506cafe11fd16a7c0cc50a
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-03 18:16:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatejdaks123"
+    storage_account_name = "jdterraformstate"
     container_name       = "tfstate"
     key                  = "aks-ha-${terraform.workspace}.tfstate"
   }
